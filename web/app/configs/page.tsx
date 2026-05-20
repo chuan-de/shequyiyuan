e1813c8b496096f074ce4918461faf95bea210e7
Bafeimao <48812850+chuan-de@users.noreply.github.com>
2026-05-20 22:45:17 +0800

    fix(web): mark entity tab pages as client components

--- web/app/configs/page.tsx
+++ web/app/configs/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { EntityManagementPage } from '@/components/business/entity-management-page';
 import { configsPageConfig } from '@/components/business/forms/entity-form-configs';
 
--- web/app/medical-records/page.tsx
+++ web/app/medical-records/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { EntityManagementPage } from '@/components/business/entity-management-page';
 import { medicalRecordsPageConfig } from '@/components/business/forms/entity-form-configs';
 
